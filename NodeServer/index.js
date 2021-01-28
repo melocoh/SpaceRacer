6b6b51afbd115309ca3886761abc0e179c363370
melocoh <melody.oh1412@gmail.com>
2021-01-27 21:46:30 -0800

    added package.json to root

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -7,99 +7,89 @@ const app = express();
 
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
+const port = process.env.PORT || 8080;
 
 let wsArray = [];
 
 var playerCount = 0;
 var maxPlayers = 4;
 
-wss.on('connection', function(ws) {
-	console.log("client joined.");
-	wsArray.push(ws);
+/* On connection */
+wss.on('connection', function (ws) {
+    console.log("client joined.");
+    wsArray.push(ws);
 
-  // send "hello world" interval
-  //const textInterval = setInterval(() => ws.send("hello world!"), 100);
+    /* Handle connected users */
+    ws.on('message', function (data) {
+        if (typeof (data) === "string") {
+            // client sent a string
+            console.log("string received from client -> '" + data + "'");
 
-  // send random bytes interval
-  //const binaryInterval = setInterval(() => ws.send(crypto.randomBytes(8).buffer), 110);
+        } else {
+            console.log("binary received from client -> " + Array.from(data).join(", ") + "");
+            HandleMessage(data, ws);
+        }
+    });
 
-  ws.on('message', function(data) {
-    if (typeof(data) === "string") {
-      // client sent a string
-      console.log("string received from client -> '" + data + "'");
-
-    } else {
-      console.log("binary received from client -> " + Array.from(data).join(", ") + "");
-	  HandleMessage(data, ws);
-    }
-  });
-
-  ws.on('close', function() {
-    console.log("client left.");
-    clearInterval(textInterval);
-    clearInterval(binaryInterval);
-  });
+    /* Handle disconnected users */
+    ws.on('close', function () {
+        console.log("client left.");
+    });
 });
 
-server.listen(8080, function() {
-  console.log('Listening on http://localhost:8080');
+/* Listen on port */
+server.listen(port, function () {
+    console.log("Server listening at port %d", port);
 });
 
-
-
-// needs to pass in a different parameter because byte array doesn't exist :'(
+/* Handles socket messages */
 function HandleMessage(mes, ws) {
-let mesArray = Array.from(mes)
+    let mesArray = Array.from(mes)
+    if (mesArray.length == 0) {
+        return;
+    }
 
+    let key = mesArray[0];
 
+    switch (key) {
+        case 1:
+            //new player
+            if (playerCount < maxPlayers) {
 
-  if (mesArray.length == 0) {
-    return;
-  }
+                let buffer = new ArrayBuffer(2);
+                let uint8View = new Uint8Array(buffer);
+                uint8View[0] = 2;
+                uint8View[1] = playerCount;
+                playerCount++;
 
-  // needs to change byte
-  let key = mesArray[0];
+                // sends player position
+                ws.send(uint8View);
+                console.log("MESSAGE SENT!!");
+            }
+            break;
+        case 2:
+            // code block
+            break;
+        case 3:
+            // onClick
+            let playerID = mesArray[1];
+            let playerClicks = mesArray[2];
+            console.log("server-receieved: car update " + playerID.toString() + " : " + playerClicks.toString());
 
+            //update the other clients
+            let buffer = new ArrayBuffer(3);
+            let uint8View = new Uint8Array(buffer);
+            uint8View[0] = mesArray[0];
+            uint8View[1] = mesArray[1];
+            uint8View[2] = mesArray[2];
 
-  switch (key) {
-    case 1:
-		//new player
-		if (playerCount < maxPlayers) {
-		  
-			let buffer = new ArrayBuffer(2);
-			let uint8View = new Uint8Array(buffer);
-			uint8View[0]=2;
-			uint8View[1]=playerCount;
-			playerCount++;
-		
-			ws.send(uint8View);
-			console.log("MESSAGE SENT!!");
-		}
-
-      break;
-    case 2:
-		// code block
-      break;
-    case 3:
-		let playerID = mesArray[1];
-		let playerClicks = mesArray[2];
-		console.log("server-receieved: car update " + playerID.toString() + " : " + playerClicks.toString());
-
-		//update the other clients
-	  	let buffer = new ArrayBuffer(3);
-		let uint8View = new Uint8Array(buffer);
-		uint8View[0] = mesArray[0];
-		uint8View[1] = mesArray[1];
-		uint8View[2] = mesArray[2];
-
-		  wsArray.forEach(sendws => {
-			  sendws.send(uint8View);
-		  });
-
-		break;
-    default:
-		// code block
-		break;
-	}
-
+            // broadcasts movement / click to all clients
+            wsArray.forEach(sendws => {
+                sendws.send(uint8View);
+            });
+            break;
+        default:
+            // code block
+            break;
+    }
 }
