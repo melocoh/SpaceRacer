0b0f2c1cc10f2eb93392bacec5329e9a33d82588
melocoh <melody.oh1412@gmail.com>
2021-01-28 07:30:28 -0800

    changed port to work on Heroku local

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server });
-//const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
-const port = process.env.PORT;
-//app.set('port', (process.env.PORT || 8080));
+//const port = process.env.PORT;
+//app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/Web'));
 
