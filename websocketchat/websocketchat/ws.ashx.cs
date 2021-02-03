832c60694e920d1cb3797bb57d7eb97527a2cfba
melocoh <melody.oh1412@gmail.com>
2021-02-02 22:04:19 -0800

    added websocket hub

---
+++ websocketchat/websocketchat/ws.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Web.WebSockets;
+
+namespace websocketchat
+{
+    /// <summary>
+    /// Summary description for ws
+    /// </summary>
+    public class ws : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+
+                context.AcceptWebSocketRequest(new TestWebSocketHandler());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
